e80ac9f89e282cd4be5d4783ee950d8add3e6233
zhanggaofan <zhanggaofan0827@gmail.com>
2025-02-04 13:40:17 +0800

    Enable arduino uart

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 #include <Arduino.h>
+// #include <HardwareSerial.h>
+// #include <Wire.h>
+// #include <SPI.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
-// HardwareSerial mySerial(2);
+// HardwareSerial mySerial(1);
+
+// arduino::UART my_uart1(0, 1);
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  // Serial1.begin(9600);
+  Serial1.setRX(29);
+  Serial1.setTX(12);
+  Serial1.begin(1250000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial1.print("XXXXXXXXXX\r\n");
+  sleep_ms(2000);
 }
 
 // put function definitions here:
